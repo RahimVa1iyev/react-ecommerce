448c24fc43c7bdd8c43dd144d70e4f63566e1313
rahimvaliyev <rahimvaliyev99@gmail.com>
2023-11-02 19:46:35 +0400

    change-url3

--- src/components/Carousel/Carousell.jsx
+++ src/components/Carousel/Carousell.jsx
@@ -15,7 +15,7 @@ const Carousell = () => {
 
    useEffect(()=>{
      const getSlider = async () =>{
-     await axios.get("https://rahimfront-001-site1.anytempurl.com//api/Sliders/all")    
+     await axios.get("http://rahimfront-001-site1.anytempurl.com/api/Sliders/all")    
                 .then(res => setItems(res.data))
                 .catch(err=> console.log("An unexpected error occured"))
     
